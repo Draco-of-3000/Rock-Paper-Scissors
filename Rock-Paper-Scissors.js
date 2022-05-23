7680cfdeb169e275860a6d117cb09ab6fbf1f5ce
Draco <kevin.tenisson@gmail.com>
2022-05-23 03:25:54 -0700

    Created playGame function to run the game for 5 rounds but it doesn't work

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -11,21 +11,66 @@ if (!isNaN(playerSelection) || playerSelection != 'ROCK' && playerSelection != '
 
 function playRound (computerSelection, playerSelection) {
     if (computerSelection == 'ROCK' && playerSelection == 'SCISSORS'){
+        computerScore++;
         return "You LOSE! Rock beats Scissors!";
     }
     else if (computerSelection == 'SCISSORS' && playerSelection == 'PAPER'){
+        computerScore++;
         return "You LOSE! Scissors beats Paper!";
     }
     else if (playerSelection == 'ROCK' && computerSelection == 'SCISSORS'){
+        playerScore++;
         return "You WIN! Rock beats Scissors!";
     }
     else if (playerSelection == 'SCISSORS' && computerSelection == 'PAPER'){
+        playerScore++;
         return "You WIN! Scissors beats Paper!";
     }
     else if (playerSelection === computerSelection){
         return "It ends in a TIE";
     }
 }
-
 const computerSelection = computerPlay();
-playRound(computerSelection, playerSelection);
+playRound(playerSelection, computerSelection);
+
+
+
+computerScore = 0;
+playerScore = 0;
+rounds = 0;
+let playerValue = playerSelection;
+let computerValue = computerSelection;
+
+function game () {
+    for (let i = 0; i < 5; i++){
+        playerValue;
+        console.log("You chose" + playerSelection);
+
+        computerValue;
+        console.log("CPU chose" + computerSelection)
+
+        if (computerScore > playerScore){
+            return `You LOST! CPU beat you ${computerScore} times`;
+        }
+        else if (playerScore > computerScore){
+            return `You WON! you beat the CPU ${playerScore} times`;
+        }
+        else {
+            return "It's a tie";
+        }    
+
+    }
+
+    if (computerScore > playerScore){
+        return `You LOST! CPU beat you ${computerScore} times`;
+    }
+    else if (playerScore > computerScore){
+        return `You WON! you beat the CPU ${playerScore} times`;
+    }
+    else {
+        return "It's a tie";
+    }    
+}
+console.log(game(playRound));
+
+
