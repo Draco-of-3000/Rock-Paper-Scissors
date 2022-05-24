d481249704a83c4ab9c2d305f7123be63247597a
Draco <kevin.tenisson@gmail.com>
2022-05-24 10:51:15 -0700

    Added Comments

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,10 +1,12 @@
+// Made an array for 3 possible answers
 let rockPaperOrScissors = ['ROCK', 'PAPER', 'SCISSORS'];
 
+// A function to make the CPU select Rock, Paper or Scissors at random
 function computerPlay() {
     return rockPaperOrScissors [Math.floor(Math.random() * rockPaperOrScissors.length)];
 }
 
-
+// Declaration of scores and return values
 let computerScore = parseInt(0);
 let playerScore = parseInt(0);
 let tiedScore = parseInt(0);
@@ -12,6 +14,7 @@ let win = "You WIN this round";
 let lose = "You LOSE this round";
 let tie = "It's a TIE GAME!"
 
+// A function that plays a single round of Rock Paper Scissors. 
 function playRound (computerSelection, playerSelection) {
     if (!isNaN(playerSelection) || playerSelection != 'ROCK' && playerSelection != 'PAPER' && playerSelection != 'SCISSORS'){
     console.log("Invalid Input");
@@ -53,17 +56,19 @@ function playRound (computerSelection, playerSelection) {
 
 }
 
+// Copying computerPlay() function into a variable
 const computerSelection = computerPlay();
 
+//A function that plays 5 rounds of Rock Paper Scissors and reports the winner and loser at the end!
 function playGame (){
     for (let i = 0; i < 5; i++){
         let playerSelection = prompt("Rock, Paper or Scissors?").toUpperCase();
         console.log(playRound(computerSelection, playerSelection));
         if (i === 4  && computerScore > playerScore){
-            return `CPU chose ${computerSelection} and you chose ${playerSelection}` + " "  + `You LOST the GAME! CPU beat you ${computerScore} times out of 5 rounds there were ${tiedScore} tie(s)`;
+            return `CPU chose ${computerSelection} and you chose ${playerSelection}` + "--- "  + `You LOST the GAME! CPU beat you ${computerScore} times out of 5 rounds there were ${tiedScore} tie(s)`;
         }
         else if (i === 4 && playerScore > computerScore){
-            return  `CPU chose ${computerSelection} and you chose ${playerSelection}` + " " + `You WON the GAME! you beat the CPU ${playerScore} times out of 5 rounds there were ${tiedScore} tie(s)`;
+            return  `CPU chose ${computerSelection} and you chose ${playerSelection}` + "--- " + `You WON the GAME! you beat the CPU ${playerScore} times out of 5 rounds there were ${tiedScore} tie(s)`;
         }
         else if (i === 4 && playerScore === computerScore){
             return `It's a tie you scored ${playerScore} out of 5, CPU scored ${computerScore} out of 5 there were ${tiedScore} tie(s)`;
